b15e3101937d68de504ed0b011524becddc23646
she0930she <she0930she@hotmail.com>
2023-05-19 08:19:38 -0700

    add eureka discovery dependency & add subscribe to registry

--- src/main/java/com/amandatao/employee/service/EmployeeServiceApplication.java
+++ src/main/java/com/amandatao/employee/service/EmployeeServiceApplication.java
@@ -2,10 +2,12 @@ package com.amandatao.employee.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
